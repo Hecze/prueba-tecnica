2d61d0e96fc4bdcf4c05d329fbd7441285fab9bf
Hecze <hectorzerrillo85@gmail.com>
2023-12-21 14:35:58 -0500

    99.9% listo xd

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
     
 
       <html lang="en">
-        <body>
+        <body >
           <Sidebar />
           {children}</body>
       </html>
